f13c2f568f128a3d5eabb030731c1cbacbf7b293
unknown <jdavid.stevens@gmail.com>
2018-10-10 14:32:57 -0600

    Rename_test

--- server/server.js
+++ server/server.js
@@ -13,11 +13,28 @@ app.use(session({
 }))
 
 let{
-SERVER_PORT
+SERVER_PORT,
+REACT_APP_DOMAIN,
+REACT_APP_CLIENT_ID,
+CLIENT_SECRET
 } = process.env;
 
 app.get('/auth/callback', async (req,res)=>{
-
+    //auth-0 sending code in req.querycode
+//=======COPY PASTE START===============
+    let payload ={
+        client_id: REACT_APP_CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: req.query.code,
+        grant_type: 'authorization_code',
+        redirect_uri: `http://${req.headers.host}/auth/callback`
+    }
+    //exchange code foer token. Token is on resWithToken.data.access_token
+    let resWithToken = await axios.post(`https://${REACT_APP_DOMAIN}/oauth/token`,payload);
+    //exchange token for user data
+    let resWithData= await axios.get(`https://${REACT_APP_DOMAIN}/userinfo/?access_token=${resWithToken.data.access_token}`)
+    console.log("Test:",resWithData.data)
+    //======COPY PASTE END======
 })
 
 app.listen(SERVER_PORT, ()=>{
